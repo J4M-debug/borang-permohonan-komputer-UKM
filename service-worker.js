4517e15f462814df71d2a408868592607e752a8f
J4M-debug <jasz@ukm.edu.my>
2025-02-06 16:02:36 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,39 @@
+const CACHE_NAME = "loan-app-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/service-worker.js",
+    "/icon.png",
+    "/icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker: Berjaya Dipasang");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-    console.log("Fetching:", event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
